c431ef99e367471220bd2d2eef50a22df84f9077
Chris Reed <chris.reed@nxp.com>
2017-01-12 17:16:48 -0600

    Simple microseconds timer.

---
+++ src/app/microseconds.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2017 Immo Software
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of the copyright holder nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#if !defined(_MICROSECONDS_H_)
+#define _MICROSECONDS_H_
+
+#include <stdint.h>
+#include "fsl_pit.h"
+
+//------------------------------------------------------------------------------
+// Definitions
+//------------------------------------------------------------------------------
+
+namespace slab {
+
+class Microseconds
+{
+public:
+
+    static void init();
+
+    static uint32_t get()
+    {
+        return (~PIT_GetCurrentTimerCount(PIT, kPIT_Chnl_0)) / s_busClock_MHz;
+    }
+
+protected:
+    static uint32_t s_busClock_MHz;
+};
+
+} // namespace slab
+
+#endif // _MICROSECONDS_H_
+//------------------------------------------------------------------------------
+// EOF
+//------------------------------------------------------------------------------
